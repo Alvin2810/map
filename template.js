000faef0b175ba5e9b68448d60a16d8a495bd81a
Alvin2810 <48338964+Alvin2810@users.noreply.github.com>
2020-04-01 18:46:01 +0930

    Add files via upload

---
+++ template.js
@@ -0,0 +1,42 @@
+
+function fuel_page() {
+   document.getElementById('fuel_page').style.display = "block";
+}
+function close_fuel_page() {
+   document.getElementById('fuel_page').style.display = "none";
+}
+
+function service_page() {
+   document.getElementById('service_page').style.display = "block";
+}
+function close_service_page() {
+   document.getElementById('service_page').style.display = "none";
+}
+
+function maintanence_page() {
+   document.getElementById('maintanence_page').style.display = "block";
+}
+
+function close_maintanence_page() {
+   document.getElementById('maintanence_page').style.display = "none";
+}
+
+function openNav1() {
+  document.getElementById("mySidenav1").style.width = "250px";
+}
+function openNav2() {
+  document.getElementById("mySidenav2").style.width = "250px";
+}
+function openNav3() {
+  document.getElementById("mySidenav3").style.width = "250px";
+}
+
+function closeNav1() {
+  document.getElementById("mySidenav1").style.width = "0";
+}
+function closeNav2() {
+  document.getElementById("mySidenav2").style.width = "0";
+}
+function closeNav3() {
+  document.getElementById("mySidenav3").style.width = "0";
+}
